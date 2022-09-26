f95091ad915bfe51720aa9d81d9d06b716c26cfb
Manuel Diaz <manu.tumbe@gmail.com>
2022-09-26 13:05:03 -0300

    Detalle servicios

--- paginas/registro.php
+++ paginas/registro.php
@@ -44,19 +44,20 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     $passConfirm = $_POST['passConfirm'];
     $img = $_FILES['imgPerfil']['name'];
     $imgTmpName = $_FILES['imgPerfil']['tmp_name'];
+    
     $maxEdad = 95;
     $minEdad = 16;
     $edad = obtener_edad_segun_fecha($fechaNacimiento);
     $codigo = $_POST['codigo'];
         
-     if($codigo == $_SESSION['codigo']){
+     if($codigo != $_SESSION['codigo']){
          unset($_SESSION['codigo']);
-         $notificacion = "CODIGO CORRECTO!!";
-        //$notificacion = "Error: El codigo de validacion es incorrecto.";
+         
+        $notificacion = "Error: El código de validación es incorrecto.";
      }else if(empty($nombreCompleto) || empty($correoCliente) || empty($telefonoCliente) || empty($dniCliente) || empty($direccionCliente) || empty($fechaNacimiento) || empty($img) || empty($pass)){
-        $notificacion = "Error: no puede dejar campos vacios.";
+        $notificacion = "Error: no puede dejar campos vacíos.";
     }else if(strlen($nombreCompleto) <= 5){
-        $notificacion = "Error: El nombre debe contener almenos 6 caracteres.";
+        $notificacion = "Error: El nombre debe contener al menos 6 caracteres.";
         $nombreError = true;
     }else if($pass != $passConfirm){
         $notificacion = "Error: Las contraseñas deben coincidir.";
@@ -68,12 +69,12 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $notificacion = "Error: El teléfono ingresado no es correcto.";
     }else{
         /* LISTAR USUARIO POR CORREO  */
-
+        unset($_SESSION['codigo']);
         $stmt = $conexion->prepare("SELECT * FROM usuarios WHERE correo = :correo");
         $stmt->execute(array(':correo' => $correoCliente));
 
         if($stmt->rowCount() > 0){
-            $notificacion = "Error: El correo ya está registrado.";
+            $notificacion = "Error: El correo ingresado ya está registrado.";
         }else{
 
             $archivo_destino = '../img/usuarios/'.$_FILES['imgPerfil']['name'];
@@ -86,8 +87,9 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             $resultado = $stmt->execute(array(':imgUsuario' => $img, ':nombre' => $nombreCompleto, ':correo' => $correoCliente,':password' => $passHash,':telefono'=> $telefonoCliente, ':dni' => $dniCliente, ':direccion' => $direccionCliente, ':fecha' => $fechaNacimiento));
 
             if($resultado){
-                
+
                 $notificacionExito = "Éxito: se ha registrado correctamente.";
+
             }
         }
     }
@@ -96,12 +98,14 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 
 ?>
 
+
+
 <main class="registrar-servicio">
 
     <seccion class="formulario py-1">
        
         <div class="container">
-            <div class="row">
+            <div class="row align-items-center">
             
                 <div class="col-4">
                     <h2 class="mb-1 text-center"><b>Formá parte de la nueva app de servicios de la Ciudad de Chivilcoy</b></h2>
@@ -112,9 +116,10 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
             <?php 
             if(isset($notificacion)){
                 echo '<p class="bg-danger text-white text-center">'.$notificacion.'</p>';
-            }else if(isset($notificacionExito)){
-                echo '<p class="bg-success text-white text-center">'.$notificacionExito.'</p>';
-            }                    
+            }else {
+
+            }
+
             ?>
 
                 <form action="registro.php" method="POST" enctype="multipart/form-data" id="formRegistro" class="row">
@@ -130,30 +135,30 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                             <input type="file" class="form-control" id="imgPerfil" name="imgPerfil">
                             <p class="text-white bg-danger msj-error">Error: Debe elegir una imagen de perfil.</p>
                         </div>
-                        <div class="mb-3">
-                            <label for="correoProveedor">Correo electrónico:</label>
-                            <input type="email" class="form-control" id="correoProveedor" name="correoCliente" required>
-                        </div>
+                        
                         <div class="mb-3">
                             <label for="telefonoProveedor">Teléfono (Ej. 2346-xxxxxx):</label>
                             <input type="text" class="form-control" id="telefonoProveedor" name="telefonoCliente" required>
                         </div>
+
                         <div class="mb-3">
-                            <label for="servicioProveedor">Tipo de servicio:</label>
-                            <select name="tipoServicio" id="servicioProveedor" class="form-select">
-                                <?php 
-                                foreach($categorias as $fila){
-                                    echo '<option value="'.$fila['idCategoria'].'">'.ucfirst($fila['categoria']).'</option>';
-                                }
-                                ?>
-                            </select>
+                            <label for="correoProveedor">Correo electrónico:</label>
+                            <input type="email" class="form-control" id="correoProveedor" name="correoCliente" required>
                         </div>
+
+                        <div class="mb-3">
+                            <label for="codigo">Código:</label>
+                            <input type="text" class="form-control" id="codigo" name="codigo" required>
+                        </div>
+                      
+                        <button type="button" id="btnCodigo" onclick="validacionCorreo()" class="btn d-grid gap-2 col-6 mx-auto boton-servicio">Solicitar código</button>
+
                     </div>
 
                     <div class="col-12 col-md-6">
                         <div class="mb-3">
                             <label for="telefonoProveedor">Dirección:</label>
-                            <input type="text" class="form-control" id="telefonoProveedor" name="direccionCliente" required>
+                            <input type="text" class="form-control" id="direccionCliente" name="direccionCliente" required>
                         </div>
                         <div class="mb-3">
                             <label for="fechaNacimiento">Fecha de nacimiento:</label>
@@ -171,16 +176,13 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
                             <label for="passConfirm">Repita su contraseña:</label>
                             <input type="password" class="form-control" id="passConfirm" name="passConfirm" required>
                         </div>
-                        <div class="mb-3">
-                            <label for="codigo">Código:</label>
-                            <input type="text" class="form-control" id="codigo" name="codigo" required>
-                        </div>
+                        
                         
                     </div>
 
                                        
-                    <button type="submit" class="btn d-grid gap-2 col-5 mx-auto boton-servicio">Enviar solicitud</button>
-                    <button type="button" onclick="validacionCorreo()" class="btn d-grid gap-2 col-5 mx-auto boton-servicio">Validar correo</button>
+                    <button type="submit" id="btnEnviar" class="d-none btn d-grid gap-2 col-5 mx-auto boton-servicio">Enviar solicitud</button>
+                    
 
                 </form>
 
@@ -190,13 +192,23 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
     </seccion>
 
 </main>
+<!-- SWEET ALERT -->
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
-<script>
+ <script>
     let errorServidor = "<?php echo (isset($notificacion)) ? $notificacion : '' ;?>";
+    let exitoServidor = "<?php echo (isset($notificacionExito)) ? $notificacionExito : '' ;?>";
+    
     if(errorServidor){
-        alert('Error');
+        alert(errorServidor);
+    }else if(exitoServidor)
+    {
+        alert(exitoServidor); 
+        window.location.href = '/proyectos/speedservice/paginas/ingresar.php';
+        
     }
 
+   
 </script>
 <script src="../js/validarRegistro.js?<?php echo time();?>"></script>
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
